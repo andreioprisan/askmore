96ea25086a0693e5c8ca9c483db861832b06230f
Andrei Oprisan <andrei.oprisan@mac.com>
2013-07-27 09:14:08 -0400

    bs 3.0

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,133 +3,93 @@
 <head>
 	<meta charset="utf-8">
 	<title>AskMore</title>
+
 	<link href="public/css/bootstrap.min.css" rel="stylesheet">
 	<link href="public/css/bootstrap-responsive.css" rel="stylesheet">
 
 
 	<style type="text/css">
-      body {
-        padding-top: 0px;
-        padding-bottom: 0px;
-      }
 
-      /* Custom container */
-      .container {
-        margin: 0 auto;
-        max-width: 100%;
-      }
-      .container > hr {
-        margin: 60px 0;
-      }
+.bs-masthead {
+	margin-top: 50px;
+}
 
-      /* Main marketing message and sign up button */
-      .jumbotron {
-        margin: 80px 0;
-        text-align: center;
-      }
-      .jumbotron h1 {
-        font-size: 100px;
-        line-height: 1;
-      }
-      .jumbotron .lead {
-        font-size: 24px;
-        line-height: 1.25;
-      }
-      .jumbotron .btn {
-        font-size: 21px;
-        padding: 14px 24px;
-      }
+.bs-docs-nav {
+background-color: #357ebd;
+}
 
-      /* Supporting marketing content */
-      .marketing {
-        margin: 60px 0;
-      }
-      .marketing p + h4 {
-        margin-top: 28px;
-      }
+.bs-masthead {
+padding-top: 100px;
+padding-bottom: 100px;
+}
 
+.bs-masthead {
+position: relative;
+padding: 30px 15px;
+text-align: center;
+}
 
-      /* Customize the navbar links to be fill the entire space of the .navbar */
-      .navbar .navbar-inner {
-        padding: 0;
-      }
-      .navbar .nav {
-        margin: 0;
-        display: table;
-        width: 100%;
-      }
-      .navbar .nav li {
-        display: table-cell;
-        width: 1%;
-        float: none;
-      }
-      .navbar .nav li a {
-        font-weight: bold;
-        text-align: center;
-        border-left: 1px solid rgba(255,255,255,.75);
-        border-right: 1px solid rgba(0,0,0,.1);
-      }
-      .navbar .nav li:first-child a {
-        border-left: 0;
-        border-radius: 3px 0 0 3px;
-      }
-      .navbar .nav li:last-child a {
-        border-right: 0;
-        border-radius: 0 3px 3px 0;
-      }
+.navbar-inverse .navbar-nav>li>a {
+color: white;
+}
+
+.navbar-brand {
+	color: white;
+}
+body {
+font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+font-size: 14px;
+line-height: 1.428571429;
+color: #333333;
+}
+
+.navbar-inverse .navbar-brand {
+color: white;
+}
     </style>
 
 </head>
 
 <html>
 
-<div class="container">
-
-	  <div class="masthead">
-        <ul class="nav nav-pills pull-right">
-          <li class="active"><a href="#">Welcome</a></li>
-          <li><a href="/plans">Plans</a></li>
-          <li><a href="/account/login">Login</a></li>
-          <li><a href="/account/signup">Sign Up</a></li>
-        </ul>
-        <h3 class="muted">ask more now - the questions you want to ask, make easy</h3>
-      </div>
-
-      <!-- Jumbotron -->
-      <div class="jumbotron">
-        <h1>social question service</h1>
-        <h2>We're making it easy to get the unanswered question to the event hosts. </h2>
-        <p class="lead">Let event attendees ask the questions they're dying to ask and have their peers vote their relevance. </p>
-        <a class="btn btn-large btn-success" href="/account/signup">Get started today for free!</a>
-      </div>
-
-      <hr>
-
-      <!-- Example row of columns -->
-      <div class="row-fluid">
-        <div class="span4">
-          <h2>Social</h2>
-          <p>Let the power of social networks work for you by creating social interactions in question up or down voting.</p>
-          <p><a class="btn" href="#">View details »</a></p>
-        </div>
-        <div class="span4">
-          <h2>Phone and SMS</h2>
-          <p>Get an event phone number that people can call in with their questions or shoot a quick question to.</p>
-          <p><a class="btn" href="#">View details »</a></p>
-       </div>
-        <div class="span4">
-          <h2>Twitter - Real time</h2>
-          <p>We'll watch Twitter for questions being answered with your event hashtag. Nothing to do on your end.</p>
-          <p><a class="btn" href="#">View details »</a></p>
-        </div>
-      </div>
-
-      <hr>
-
-      <div class="footer">
-        <p>© AskMoreNow 2013</p>
-      </div>
-
+<div class="navbar navbar-inverse navbar-fixed-top bs-docs-nav">
+  <div class="container">
+    <a href="../" class="navbar-brand">ask more now</a>
+    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <div class="nav-collapse collapse bs-navbar-collapse">
+      <ul class="nav navbar-nav pull-right">
+        <li>
+          <a href="/plans">Plans</a>
+        </li>
+        <li class="">
+          <a href="/account/login">Login</a>
+        </li>
+        <li>
+          <a href="/account/signup">Sign Up</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</div>
+
+<div class="bs-masthead">
+  <div class="container">
+    <h1>social question service</h1>
+    <h2>We're making it easy to get the unanswered question to the event hosts. </h2>
+    <p class="lead">Let event attendees ask the questions they're dying to ask and have their peers vote their relevance. </p>
+    <a class="btn btn-primary btn-large" href="/account/signup">Get started today for free!</a>
+  </div>
+</div>
+
+
+<div class="bs-old-docs">
+  <div class="container">
+  	© AskMoreNow 2013
+  </div>
+</div>
 
 </html>
